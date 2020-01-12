7f50f1dad589e9433d068ed402e657fc62623db9
Enes Aydogan <enes35ihsan@gmail.com>
2020-01-12 21:24:30 +0000

    initial commit

---
+++ main.js
@@ -0,0 +1,153 @@
+var fs = require('fs');
+var express = require("express");
+var app = express();
+var http = require("http");//Server(app);
+var bodyParser = require("body-parser");
+
+app.use(bodyParser.json());
+app.use(express.static(__dirname + '/'));
+
+var port = 80;
+var vals = {
+    '3':'visafree',
+    '2':'visaeta',
+    '1':'visaonarrival',
+    '0':'visarequired',
+    '-1':'self'
+};
+
+// Converts country code to country name. i.e. TR = Turkey
+function codeToName(){
+    var obj = {};
+    var data = fs.readFileSync('countrycodes.txt', 'utf8').split('\r\n');
+    for (i=0;i<data.length;i++){
+        var current = data[i].split(',');
+        obj[current[1]] = current[0];
+    }
+    return obj;
+}
+
+// Converts country name to country code. i.e. Bulgaria = BG
+function nameToCode(){
+    var obj = {};
+    var data = fs.readFileSync('countrycodes.txt', 'utf8').split('\r\n');
+    for (i=0;i<data.length;i++){
+        var current = data[i].split(',');
+        obj[current[0]] = current[1];
+    }
+    return obj;
+}
+
+function data(){
+    var data = fs.readFileSync('passport-index-dataset/passport-index-tidy.csv', 'utf8').split('\r\n');
+    return data;
+}
+
+function getCountries(){
+    var countries = [];
+    
+    // Passport,Destination,Value (3=visa_free,2=visa_eta,1=visa_on_arrival,0=visa required,-1=pass=dest)
+    var contents = data();
+    count = 0;
+    for(i=0;i<contents.length;i++){
+        if(i % 199 == 0){
+            var pass = contents[i].split(',')[0],
+                des = contents[i].split(',')[1],
+                val = contents[i].split(',')[2];
+            //console.log(pass);
+            countries.push(pass);
+        }
+    }
+    
+    return countries;
+}
+
+function getPass(country){
+    var objd = {
+        'visarequired':[],
+        'visaonarrival':[],
+        'visaeta':[],
+        'visafree':[],
+        'self':[]
+    };
+    var index = getCountries().indexOf(country);
+    var contents = data();
+    var iStart = index * 199;
+    //console.log(index);
+    var iEnd = (index + 1) * 199;
+    for(i=iStart;i<iEnd;i++){
+        //console.log(iStart);
+        var pass = contents[i].split(',')[0],
+            des = contents[i].split(',')[1],
+            val = contents[i].split(',')[2];
+        //console.log(contents[i].split(',')[1]);
+        //objd[val]=[];
+        objd[vals[val]].push(des);
+    }
+    //console.log(objd);
+    return objd;
+}
+
+function classify(){
+    var cs = getCountries();
+    var objs = {};
+    /*
+    var vfree = [];
+    var veta = [];
+    var vona = [];
+    var vreq = [];
+    */
+    var contents = data()    
+    cs.forEach(function(element) {
+        //console.log(element);
+        objs[element] = getPass(element);
+    });
+    
+    return objs;
+}
+
+function addLog(c){
+    var path = 'log.txt';
+    
+    fs.appendFile("log.txt", c, function(err) {
+        if(err) {
+            return console.log(err);
+        }
+    }); 
+        
+}
+
+app.get('/', function(req, res){
+    res.sendFile(__dirname + '/maps.html');
+})
+
+app.get('/codes', function(req, res){
+    res.send(JSON.stringify(nameToCode()));
+})
+
+app.get('/country/:c', function(req, res){
+    var monthNames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+    var ccodes = codeToName();
+    var pass = ccodes[req.params.c.toUpperCase()];
+    console.log(pass);
+    var d = new Date
+    addLog("[" + d.getDay() + " " + monthNames[d.getMonth()] + " " + d.getFullYear() + ", " + d.getHours() + ":" + d.getMinutes() + ", "+d.getSeconds()+"] "+pass+"\r\n")
+    res.send(JSON.stringify(getPass(pass)));
+})
+
+app.set('port', process.env.PORT || 80);
+app.set('host', process.env.HOST || '0.0.0.0');
+
+http.createServer(app).listen(app.get('port'), app.get('host'), function(){
+  console.log("Express server listening on port " + app.get('port'));
+});
+
+
+// Server and port settings.
+//http.listen(port, '192.168.1.1', function(){
+//    console.log('listening on *: ' + port);
+//});
+
+
+
+// 2754 1398
---
+++ script.js
@@ -0,0 +1,104 @@
+
+var cc = '';//countryCodes();
+countryCodes();
+
+function paint(c, color){
+	var f = document.getElementsByClassName(c);
+	for (i=0;i<f.length;i++){
+    	f[i].style.fill = color;
+	}
+}
+
+function getB(c){
+    var x = new XMLHttpRequest();
+    x.onreadystatechange=function(){
+        if (x.readyState==4 && x.status==200){
+            var passdata = JSON.parse(x.responseText);
+            paintMap(c, passdata);
+        }
+    }
+    x.open('GET', '/country/' + c, true);
+    x.setRequestHeader('Content-Type', 'application/json');
+    x.send();
+}
+    
+function countryCodes(){
+    var ret = '';
+    var x = new XMLHttpRequest();
+    x.onreadystatechange=function(){
+        if (x.readyState==4){ // && x.status==304){
+            cc = JSON.parse(x.responseText);
+        }
+    }
+    x.open('GET', '/codes', true);
+    x.setRequestHeader('Content-Type', 'application/json');
+    x.send();
+    
+}
+
+function paintMap(c, ree){
+    //paint(cc[ree['self']].toLowerCase(), 'orange');
+    var vf = ree['visafree'];
+    
+    vf.forEach(function(i){
+        if (cc[i] == undefined){
+            return 0;
+            //console.log('an error occured');
+        } else {
+            //console.log(cc[i].toLowerCase());
+            paint(cc[i].toLowerCase(), 'green');
+        }
+    })
+    
+    veta = ree['visaeta']
+    veta.forEach(function(i){
+        if (cc[i] == undefined){
+            return 0;
+            //console.log('an error occured');
+        } else {
+            //console.log(cc[i].toLowerCase());
+            paint(cc[i].toLowerCase(), 'beffb3');
+        }
+    })
+        
+    von = ree['visaonarrival']
+    von.forEach(function(i){
+        if (cc[i] == undefined){
+            return 0;
+            //console.log('an error occured');
+        } else {
+            //console.log(cc[i].toLowerCase());
+            paint(cc[i].toLowerCase(), '8ed498');    // 8ed498
+        }
+    })
+    
+    // comment here to get a combined result.
+    vr = ree['visarequired']
+    vr.forEach(function(i){
+        if (cc[i] == undefined){
+            return 0;
+            //console.log('an error occured');
+        } else {
+            //console.log(cc[i].toLowerCase());
+            paint(cc[i].toLowerCase(), 'c0c0c0');    // 8ed498
+        }
+    })
+    
+    if (document.getElementById(c).className['animVal'].indexOf(' eu') > 0){ paint('eu', '003399'); }
+    
+    paint(cc[ree['self']].toLowerCase(), 'orange');
+    
+    
+}
+
+document.addEventListener('click', function(e) {
+    e = e || window.event;
+    var target = e.target || e.srcElement,
+        text = target.textContent || target.innerText;
+    if (target.parentNode.tagName == 'svg'){
+        getB(target.id)
+    } else {
+        getB(target.parentNode.id);
+    }
+    
+}, false);
